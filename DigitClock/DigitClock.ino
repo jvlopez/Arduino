827d627c20c56d538c303f326a1c1fabdd10e195
Antoine Hauck <antoine.hauck@bbv.ch>
2016-01-16 22:45:45 +0100

    Clean Code

--- DigitClock/DigitClock.ino
+++ DigitClock/DigitClock.ino
@@ -4,7 +4,6 @@
 #include <EEPROM.h>
 #include <BH1750.h>
 #include <FastLED.h>
-#include "ardprintf.h"
 
 #define DEBUG_OVER_SERIAL
 #ifdef DEBUG_OVER_SERIAL
@@ -24,41 +23,23 @@
 #define BTN_MINUTES_PIN 3
 #define BTN_BRIGHTNESS_PIN 4
 #define BTN_COLOR_PIN 5
-#define DATA_PIN 6  // Data pin for led comunication
+#define LED_DATA_PIN 6
 #define EEPROM_SETTINGS_ADDR 0
-#define BRIGHTNESS_MAX_LUX 20000
-#define BRIGHTNESS_THRESHOLD 10
-#define BRIGHTNESS_OFF 0
-#define BRIGHTNESS_LOWEST 10
-#define BRIGHTNESS_DIMMED 64
-#define BRIGHTNESS_MEDIUM 128
-#define BRIGHTNESS_BRIGHT 192
-#define BRIGHTNESS_FULL 255
+#define LIGHTSENSOR_HIGH_LUX 20000
+#define LED_LOWEST_LUMINOSITY 10
+#define LED_HIGHEST_LUMINOSITY 255
 #define BLACK CHSV(0, 255, 0)
 #define FRAMES_PER_SECOND  24
 #define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
-#define INITIAL_SETTINGS { FULL, 0, CHSV(0, 255, 255)}
-
-CRGB leds[NUM_LEDS]; // Define the array of the LEDs strip
-byte digits[10][7] = { {0,1,1,1,1,1,1},  // Digit 0
-					 {0,1,0,0,0,0,1},   // Digit 1
-					 {1,1,1,0,1,1,0},   // Digit 2
-					 {1,1,1,0,0,1,1},   // Digit 3
-					 {1,1,0,1,0,0,1},   // Digit 4
-					 {1,0,1,1,0,1,1},   // Digit 5
-					 {1,0,1,1,1,1,1},   // Digit 6
-					 {0,1,1,0,0,0,1},   // Digit 7
-					 {1,1,1,1,1,1,1},   // Digit 8
-					 {1,1,1,1,0,1,1} };  // Digit 9 | 2D Array for numbers on 7 segment
-uint8_t digitOffset[4] = { 23, 16, 7, 0 };
+#define INITIAL_SETTINGS { FULL, 0, getColor(RED) }
 
 struct Settings {
-	uint8_t brightnessMode;
+	uint8_t luminosityMode;
 	uint8_t colorPattern;
 	CHSV color;
 };
 
-enum BrightnessMode {
+enum LuminosityMode {
 	SENSOR_DRIVEN,
 	DIMMED,
 	MEDIUM,
@@ -68,77 +49,106 @@ enum BrightnessMode {
 	SIZE
 };
 
+enum ColorPatterns {
+	DYNAMIC,
+	HOLD,
+	RED,
+	GREEN,
+	BLUE,
+	WHITE,
+	PATTERNS_SIZE
+};
+
 Settings settings = INITIAL_SETTINGS;
 bool dotIsVisible = true;
 int timeChanged = 0;
-uint8_t brightness = 0;
-CHSV color = settings.color;
+uint8_t luminosityValues[SIZE];
+uint8_t currentLuminosity = 0;
 BH1750 lightMeter;
 
-// List of patterns to cycle through. Each is defined as a separate function below.
-typedef void(*SimplePatternList[])();
-SimplePatternList colorPatternList = { colorWheel, keepColor, red, green, blue, white };
+ColorPatterns colorPatterns[] = { DYNAMIC, HOLD, RED, GREEN, BLUE, WHITE };
+CRGB leds[NUM_LEDS]; // Define the color array of the LED strip
+byte digits[10][7] = {	{0,1,1,1,1,1,1},   // Digit 0
+						{0,1,0,0,0,0,1},   // Digit 1
+						{1,1,1,0,1,1,0},   // Digit 2
+						{1,1,1,0,0,1,1},   // Digit 3
+						{1,1,0,1,0,0,1},   // Digit 4
+						{1,0,1,1,0,1,1},   // Digit 5
+						{1,0,1,1,1,1,1},   // Digit 6
+						{0,1,1,0,0,0,1},   // Digit 7
+						{1,1,1,1,1,1,1},   // Digit 8
+						{1,1,1,1,0,1,1} }; // Digit 9 | 2D Array for numbers on 7 segment
+uint8_t digitOffset[] = { 23, 16, 7, 0 };
 
 void setup(){
 	DEBUG_OPEN_SERIAL(9600);
 	Wire.begin();
 	lightMeter.begin(BH1750_CONTINUOUS_LOW_RES_MODE);
-	getOrInitializeSettings();
-	color = settings.color;
-	setBrightness(BrightnessModeToValue(settings.brightnessMode));
-	LEDS.addLeds<WS2811, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);; // Set LED strip type
+	createLuminosityDefinition();
+	getSettings();
+	LEDS.addLeds<WS2811, LED_DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
 	pinMode(BTN_BRIGHTNESS_PIN, INPUT_PULLUP);
 	pinMode(BTN_COLOR_PIN, INPUT_PULLUP);
 	pinMode(BTN_HOURS_PIN, INPUT_PULLUP);
 	pinMode(BTN_MINUTES_PIN, INPUT_PULLUP);
 }
 
-void changeBrightnessMode()
+CHSV getColor(uint8_t forPattern)
 {
-	settings.brightnessMode = (settings.brightnessMode + 1) % SIZE;
+	switch (forPattern)
+	{
+		case DYNAMIC:
+			settings.color.hue += 1;
+		case HOLD:
+			return CHSV(settings.color.hue, 255, 255);
+		case RED:
+			return CHSV(0, 255, 255);
+		case GREEN:
+			return CHSV(96, 255, 255);
+		case BLUE:
+			return CHSV(160, 255, 255);
+		case WHITE: 
+		default:
+			return CHSV(settings.color.hue, 0, 255);
+	}
+}
+
+void createLuminosityDefinition()
+{
+	luminosityValues[SENSOR_DRIVEN] = 0;
+	luminosityValues[DIMMED] = 64;
+	luminosityValues[MEDIUM] = 128;
+	luminosityValues[BRIGHT] = 192;
+	luminosityValues[FULL] = 255;
+	luminosityValues[OFF] = 0;
+}
+
+void changeLuminosityMode()
+{
+	settings.luminosityMode = (settings.luminosityMode + 1) % SIZE;
 	persistSettings();
-	setBrightness(BrightnessModeToValue(settings.brightnessMode));
 }
 
 void changeColorMode()
 {
-	settings.colorPattern = (settings.colorPattern + 1) % ARRAY_SIZE(colorPatternList);
-	colorPatternList[settings.colorPattern]();
-	settings.color = color;
+	settings.colorPattern = (settings.colorPattern + 1) % ARRAY_SIZE(colorPatterns);
+	updateLedDigits();
 	persistSettings();
 }
 
-uint8_t BrightnessModeToValue(uint8_t brightnessMode)
+void updateLedLuminosity()
 {
-	switch (brightnessMode)
+	uint8_t luminosity;
+	settings.luminosityMode == SENSOR_DRIVEN ? luminosity = luminosityValueBySensor() : luminosity = luminosityValues[settings.luminosityMode];
+	if (currentLuminosity != luminosity)
 	{
-		case SENSOR_DRIVEN:
-			return BrightnessValueBySensor();
-		case DIMMED:
-			return BRIGHTNESS_DIMMED;
-		case MEDIUM:
-			return BRIGHTNESS_MEDIUM;
-		case BRIGHT:
-			return BRIGHTNESS_BRIGHT;
-		case OFF:
-			return BRIGHTNESS_OFF;
-		default:
-			return BRIGHTNESS_FULL;
+		DEBUG_PRINT_FORMATTED_LINE("Luminosity changed %d --> %d", currentLuminosity, luminosity);
+		currentLuminosity = luminosity;
+		LEDS.setBrightness(currentLuminosity);
 	}
 }
 
-void AdjustBrightnessToLightIntensity()
-{
-	uint8_t sensorValue = BrightnessValueBySensor();
-	if (brightness != sensorValue)
-	{
-		DEBUG_PRINT_FORMATTED_LINE("Brightness changed %d --> %d", brightness, sensorValue);
-		brightness = sensorValue;
-		setBrightness(brightness);
-	}
-}
-
-void HandleButtons()
+void handleButtonInteraction()
 {
 	int btnHours = digitalRead(BTN_HOURS_PIN);
 	int btnMinutes = digitalRead(BTN_MINUTES_PIN);
@@ -163,41 +173,13 @@ void HandleButtons()
 		}
 		else if (btnBrightness == LOW)
 		{
-			changeBrightnessMode();
+			changeLuminosityMode();
 		};
-
 		delay(400);
 	}
 }
 
-void keepColor() { }
-
-void colorWheel()
-{
-	color = CHSV(++color.hue, 255, 255);
-}
-
-void red()
-{
-	color = CHSV(0, 255, 255);
-}
-
-void green()
-{
-	color = CHSV(96, 255, 255);
-}
-
-void blue()
-{
-	color = CHSV(160, 255, 255);
-}
-void white()
-{
-	color = CHSV(color.hue, 0, 255);
-}
-
-
-int GetTime() {
+int getTime() {
 	tmElements_t Now;
 	RTC.read(Now);
 	printTime(Now);
@@ -205,34 +187,28 @@ int GetTime() {
 	return (Now.Hour * 100 + Now.Minute);
 };
 
-uint8_t BrightnessValueBySensor()
+uint8_t luminosityValueBySensor()
 {
 	uint16_t lightSensor = lightMeter.readLightLevel();
-	if (lightSensor > BRIGHTNESS_MAX_LUX)
-	{
-		lightSensor = BRIGHTNESS_MAX_LUX;
-	}
-	return map(lightSensor, BRIGHTNESS_MAX_LUX, 0, BRIGHTNESS_LOWEST, BRIGHTNESS_FULL);
+	lightSensor = min(lightSensor, LIGHTSENSOR_HIGH_LUX);
+	return map(lightSensor, LIGHTSENSOR_HIGH_LUX, 0, LED_LOWEST_LUMINOSITY, LED_HIGHEST_LUMINOSITY);
 }
 
-void setBrightness(uint8_t value)
+void updateLedDigits() 
 {
-	LEDS.setBrightness(value);
-}
-
-void TimeToLEDArray() {
-	int Now = GetTime();
+	// Update digit color according to current color pattern
+	settings.color = getColor(colorPatterns[settings.colorPattern]);
+	int Now = getTime();
 	int cursor = NUM_LEDS;
 
-	dotIsVisible ? (leds[14] = color) : (leds[14] = BLACK);
-	dotIsVisible ? (leds[15] = color) : (leds[15] = BLACK);
+	dotIsVisible ? (leds[14] = leds[15] = settings.color) : (leds[14] = leds[15] = BLACK);
 
 	for (int i = 0; i < 4; i++)
 	{
 		int digit = Now % 10; // get last digit in time
 		int cursor = digitOffset[i];
 		for (int k = 0; k <= 6; k++) {
-			if (digits[digit][k] == 1) { leds[cursor] = color; }
+			if (digits[digit][k] == 1) { leds[cursor] = settings.color; }
 			else if (digits[digit][k] == 0) { leds[cursor] = BLACK; };
 			cursor++;
 		};
@@ -249,10 +225,10 @@ void printTime(tmElements_t time)
 	}
 }
 
-void getOrInitializeSettings()
+void getSettings()
 {
 	EEPROM.get(EEPROM_SETTINGS_ADDR, settings);
-	if (settings.colorPattern >= ARRAY_SIZE(colorPatternList))
+	if (settings.colorPattern >= ARRAY_SIZE(colorPatterns))
 	{
 		DEBUG_PRINT_LINE("First run. Set initial settings.");
 		settings = INITIAL_SETTINGS;
@@ -269,25 +245,18 @@ void persistSettings()
 
 void printSettings()
 {
-	DEBUG_PRINT_FORMATTED_LINE("[Settings] Color Mode: %d HSV: [%d,%d,%d], Brightness Mode: %d", settings.colorPattern, settings.color.hue, settings.color.saturation, settings.color.value, settings.brightnessMode);
+	DEBUG_PRINT_FORMATTED_LINE("[Settings] Color Mode: %d HSV: [%d,%d,%d], Brightness Mode: %d", settings.colorPattern, settings.color.hue, settings.color.saturation, settings.color.value, settings.luminosityMode);
 }
 
 void loop()
 {
 	EVERY_N_MILLISECONDS(200)
 	{
-		if (settings.brightnessMode == SENSOR_DRIVEN)
-		{
-			AdjustBrightnessToLightIntensity();
-		}
+		updateLedLuminosity();
 	}
-	
-	HandleButtons();
-	// Call the current pattern function once, updating the 'leds' array
-	colorPatternList[settings.colorPattern]();
-	TimeToLEDArray();
-	// send the 'leds' array out to the actual LED strip
+	handleButtonInteraction();
+	// Update the color array of the LEDs to represent the current time & color pattern
+	updateLedDigits();
 	FastLED.show();
-	// insert a delay to keep the framerate modest
 	FastLED.delay(1000 / FRAMES_PER_SECOND);
 }
