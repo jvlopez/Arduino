f36878199f5ebab67f5ad0670e226db4265d5c05
Antoine Hauck <antoine.hauck@bbv.ch>
2016-01-18 07:38:43 +0100

    - Showing text on display.
    - Clean code.

--- DigitClock/DigitClock.ino
+++ DigitClock/DigitClock.ino
@@ -19,19 +19,24 @@
 
 #define NUM_LEDS 30 // Number of LED controllers (3 LEDS per controller)
 #define COLOR_ORDER RGB  // Define LED strip color order
+#define BTN_ADD_HOUR_PRESSED(x) (x >> 3 & 1) > 0
+#define BTN_ADD_MINUTE_PRESSED(x) (x >> 2 & 1) > 0
+#define BTN_CHANGE_LUMINOSITY_PRESSED(x) (x >> 1 & 1) > 0
+#define BTN_CHANGE_COLOR_PRESSED(x) (x & 1) > 0
 #define BTN_HOURS_PIN 2
 #define BTN_MINUTES_PIN 3
 #define BTN_BRIGHTNESS_PIN 4
 #define BTN_COLOR_PIN 5
 #define LED_DATA_PIN 6
 #define EEPROM_SETTINGS_ADDR 0
+#define AUTO_ADJUST 0
 #define LIGHTSENSOR_HIGH_LUX 20000
 #define LED_LOWEST_LUMINOSITY 10
 #define LED_HIGHEST_LUMINOSITY 255
 #define BLACK CHSV(0, 255, 0)
 #define FRAMES_PER_SECOND  24
 #define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
-#define INITIAL_SETTINGS { FULL, 0, getColor(RED) }
+#define INITIAL_SETTINGS { LED_HIGHEST_LUMINOSITY, 0, getColor(RED) }
 
 struct Settings {
 	uint8_t luminosityMode;
@@ -39,14 +44,14 @@ struct Settings {
 	CHSV color;
 };
 
-enum LuminosityMode {
-	SENSOR_DRIVEN,
-	DIMMED,
-	MEDIUM,
-	BRIGHT,
-	FULL,
-	OFF,
-	SIZE
+struct SymbolSegments {
+	byte segments[7];
+	char representation;
+};
+
+struct LuminositySettings {
+	byte luminosityValue;
+	byte representation[4];
 };
 
 enum ColorPatterns {
@@ -59,52 +64,48 @@ enum ColorPatterns {
 	PATTERNS_SIZE
 };
 
-enum Letters { A, F, G, H, I, L, O, T, U, _, LETTERS_SIZE };
+enum Letters { A = 10, F, G, H, I, L, O, T, U, _ };
 
-Settings settings = INITIAL_SETTINGS;
-bool dotIsVisible = true;
-tmElements_t previousTime, currentTime;
-uint8_t luminosityValues[SIZE];
-uint8_t currentLuminosity = 0;
-BH1750 lightMeter;
+SymbolSegments symbols[] = { { { 0,1,1,1,1,1,1 }, '0'},
+							 { { 0,1,0,0,0,0,1 }, '1'},
+							 { { 1,1,1,0,1,1,0 }, '2'},
+							 { { 1,1,1,0,0,1,1 }, '3'},
+							 { { 1,1,0,1,0,0,1 }, '4'},
+							 { { 1,0,1,1,0,1,1 }, '5'},
+							 { { 1,0,1,1,1,1,1 }, '6'},
+							 { { 0,1,1,0,0,0,1 }, '7'},
+							 { { 1,1,1,1,1,1,1 }, '8'},
+							 { { 1,1,1,1,0,1,1 }, '9'},
+							 { { 1,1,1,1,1,0,1 }, 'A'},
+							 { { 1,0,1,1,1,0,0 }, 'F'},
+							 { { 1,0,1,1,1,1,1 }, 'G'},
+							 { { 1,1,0,1,1,0,1 }, 'H'},
+							 { { 0,0,0,1,1,0,0 }, 'I'},
+							 { { 0,0,0,1,1,1,0 }, 'L'},
+							 { { 0,1,1,1,1,1,1 }, 'O'},
+							 { { 1,0,0,1,1,1,0 }, 't'},
+							 { { 0,1,0,1,1,1,1 }, 'U'},
+							 { { 0,0,0,0,0,0,0 }, ' '} };
+
+LuminositySettings luminositySetting[] =  { { 0,	{ A,U,T,O } },
+											{ 64,	{ L,O,_,_ } }, 
+											{ 128,	{ H,A,L,F } }, 
+											{ 192,	{ H,I,G,H } }, 
+											{ 255,	{ F,U,L,L } },
+											{ 0,	{ O,F,F,_ } } };
 
 ColorPatterns colorPatterns[] = { DYNAMIC, HOLD, RED, GREEN, BLUE, WHITE };
+uint8_t digitOffset[] = { 0, 7, 16, 23 };
 CRGB leds[NUM_LEDS]; // Define the color array of the LED strip
-byte digits[10][7] = {	{0,1,1,1,1,1,1},   // Digit 0
-						{0,1,0,0,0,0,1},   // Digit 1
-						{1,1,1,0,1,1,0},   // Digit 2
-						{1,1,1,0,0,1,1},   // Digit 3
-						{1,1,0,1,0,0,1},   // Digit 4
-						{1,0,1,1,0,1,1},   // Digit 5
-						{1,0,1,1,1,1,1},   // Digit 6
-						{0,1,1,0,0,0,1},   // Digit 7
-						{1,1,1,1,1,1,1},   // Digit 8
-						{1,1,1,1,0,1,1} }; // Digit 9 | 2D Array for numbers on 7 segment
-byte letters[LETTERS_SIZE][7] = {{1,1,1,1,1,0,1},   // Digit A
-						{1,0,1,1,1,0,0},   // Digit F
-						{1,0,1,1,1,1,1},   // Digit G
-						{1,1,0,1,1,0,1},   // Digit H
-						{0,0,0,1,1,0,0},   // Digit I
-						{0,0,0,1,1,1,0},   // Digit L
-						{0,1,1,1,1,1,1},   // Digit O
-						{1,0,0,1,1,1,0},   // Digit t
-						{0,1,0,1,1,1,1},   // Digit U 
-						{0,0,0,0,0,0,0} }; // Digit shows nothing | 2D Array for letters on 7 segment
-
-byte luminosityText[SIZE][4] = {{A,U,T,O},
-								{L,O,_,_},
-								{H,A,L,F},
-								{H,I,G,H},
-								{F,U,L,L},
-								{O,F,F,_}};
-
-uint8_t digitOffset[] = { 23, 16, 7, 0 };
+Settings settings = INITIAL_SETTINGS;
+tmElements_t previousTime, currentTime;
+uint8_t currentLuminosity = 0;
+BH1750 lightMeter;
 
 void setup(){
 	DEBUG_OPEN_SERIAL(9600);
 	Wire.begin();
 	lightMeter.begin(BH1750_CONTINUOUS_LOW_RES_MODE);
-	createLuminosityDefinition();
 	getSettings();
 	LEDS.addLeds<WS2811, LED_DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
 	pinMode(BTN_BRIGHTNESS_PIN, INPUT_PULLUP);
@@ -133,34 +134,18 @@ CHSV getColor(uint8_t forPattern)
 	}
 }
 
-void createLuminosityDefinition()
-{
-	luminosityValues[SENSOR_DRIVEN] = 0;
-	luminosityValues[DIMMED] = 64;
-	luminosityValues[MEDIUM] = 128;
-	luminosityValues[BRIGHT] = 192;
-	luminosityValues[FULL] = 255;
-	luminosityValues[OFF] = 0;
-}  
-
 void changeLuminosityMode()
 {
-	settings.luminosityMode = (settings.luminosityMode + 1) % SIZE;
-	showLuminosityModeText();
-	persistSettings();
-}
-
-void changeColorMode()
-{
-	settings.colorPattern = (settings.colorPattern + 1) % ARRAY_SIZE(colorPatterns);
-	updateLedDigits();
-	persistSettings();
+	settings.luminosityMode = (settings.luminosityMode + 1) % ARRAY_SIZE(luminositySetting);
+	LEDS.setBrightness(LED_HIGHEST_LUMINOSITY);
+	updateLedDigits(luminositySetting[settings.luminosityMode].representation, false);
+	delay(1000);
 }
 
 void updateLedLuminosity()
 {
 	uint8_t luminosity;
-	settings.luminosityMode == SENSOR_DRIVEN ? luminosity = luminosityValueBySensor() : luminosity = luminosityValues[settings.luminosityMode];
+	settings.luminosityMode == AUTO_ADJUST ? luminosity = luminosityValueBySensor() : luminosity = luminositySetting[settings.luminosityMode].luminosityValue;
 	if (currentLuminosity != luminosity)
 	{
 		DEBUG_PRINT_FORMATTED_LINE("Luminosity changed %d --> %d", currentLuminosity, luminosity);
@@ -169,52 +154,30 @@ void updateLedLuminosity()
 	}
 }
 
-uint8_t * showLuminosityModeText()
-{
-	leds[14] = leds[15] = BLACK;
-	
-	for (int i = 0; i < 4; i++)
-	{
-		byte letter = luminosityText[settings.luminosityMode][3-i];
-		int cursor = digitOffset[i];
-		for (int k = 0; k <= 6; k++) {
-			if (letters[letter][k] == 1) { leds[cursor] = settings.color; }
-			else if (letters[letter][k] == 0) { leds[cursor] = BLACK; };
-			cursor++;
-		};
-	}
-	LEDS.setBrightness(LED_HIGHEST_LUMINOSITY);
-	FastLED.show();
-	delay(1000);
-}
-
 void handleButtonInteraction()
 {
-	int btnHours = digitalRead(BTN_HOURS_PIN);
-	int btnMinutes = digitalRead(BTN_MINUTES_PIN);
-	int btnColorMode = digitalRead(BTN_COLOR_PIN);
-	int btnBrightness = digitalRead(BTN_BRIGHTNESS_PIN);
-
-	if (btnHours == LOW || btnMinutes == LOW || btnColorMode == LOW || btnBrightness == LOW)
+	byte buttonsPressed = (~digitalRead(BTN_HOURS_PIN) << 3 | ~digitalRead(BTN_MINUTES_PIN) << 2 | ~digitalRead(BTN_BRIGHTNESS_PIN) << 1 | ~digitalRead(BTN_COLOR_PIN));
+	if(buttonsPressed > 0)
 	{
-		tmElements_t Now;
-		RTC.read(Now);
-		if (btnHours == LOW) {
-			Now.Hour == 23 ? Now.Hour = 0 : Now.Hour++;
-			RTC.write(Now);
-		}
-		else if (btnMinutes == LOW) {
-			Now.Minute == 59 ? Now.Minute = 0 : Now.Minute++;
-			RTC.write(Now);
-		}
-		else if (btnColorMode == LOW)
+		if (BTN_ADD_HOUR_PRESSED(buttonsPressed))
 		{
-			changeColorMode();
+			currentTime.Hour = (++currentTime.Hour % 24);
+			RTC.write(currentTime);
 		}
-		else if (btnBrightness == LOW)
+		else if (BTN_ADD_MINUTE_PRESSED(buttonsPressed))
+		{
+			currentTime.Minute = (++currentTime.Minute % 60);
+			RTC.write(currentTime);
+		}
+		else if (BTN_CHANGE_LUMINOSITY_PRESSED(buttonsPressed))
 		{
 			changeLuminosityMode();
-		};
+		}
+		else if (BTN_CHANGE_COLOR_PRESSED(buttonsPressed))
+		{
+			settings.colorPattern = (settings.colorPattern + 1) % ARRAY_SIZE(colorPatterns);
+		}
+		persistSettings();
 		delay(400);
 	}
 }
@@ -226,26 +189,34 @@ uint8_t luminosityValueBySensor()
 	return map(lightSensor, LIGHTSENSOR_HIGH_LUX, 0, LED_LOWEST_LUMINOSITY, LED_HIGHEST_LUMINOSITY);
 }
 
-void updateLedDigits() 
+void showTime() 
 {
 	settings.color = getColor(colorPatterns[settings.colorPattern]);
 	int Now = (currentTime.Hour * 100 + currentTime.Minute);
+	byte timeDigits[4];
+	timeDigits[3] = Now % 10;
+	timeDigits[2] = (Now /= 10) % 10;
+	timeDigits[1] = (Now /= 10) % 10;
+	timeDigits[0] = (Now /= 10) % 10;
+	updateLedDigits(timeDigits, (currentTime.Second % 2 == 0));
+};
+
+void updateLedDigits(byte symbolsToShow[4], bool showDots)
+{
 	int cursor = NUM_LEDS;
-	dotIsVisible = (currentTime.Second % 2 == 0);
-	dotIsVisible ? (leds[14] = leds[15] = settings.color) : (leds[14] = leds[15] = BLACK);
+	leds[14] = leds[15] = showDots ? settings.color : BLACK;
 
 	for (int i = 0; i < 4; i++)
 	{
-		int digit = Now % 10; // get last digit in time
 		int cursor = digitOffset[i];
 		for (int k = 0; k <= 6; k++) {
-			if (digits[digit][k] == 1) { leds[cursor] = settings.color; }
-			else if (digits[digit][k] == 0) { leds[cursor] = BLACK; };
+			bool segmentActive = (symbols[symbolsToShow[i]].segments[k] == 1);
+			leds[cursor] = segmentActive ? settings.color : BLACK;
 			cursor++;
 		};
-		Now /= 10;
 	}
-};
+	FastLED.show();
+}
 
 void printTime()
 {
@@ -289,7 +260,6 @@ void loop()
 	RTC.read(currentTime);
 	printTime();
 	// Update the color array of the LEDs to represent the current time & color pattern
-	updateLedDigits();
-	FastLED.show();
+	showTime();
 	FastLED.delay(1000 / FRAMES_PER_SECOND);
 }
